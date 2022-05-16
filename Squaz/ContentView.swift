88d3a5913fe6dcfde98fefd25568d489f411c450
Emmanuel Mensah <41510090+Emmaxp183@users.noreply.github.com>
2022-05-16 21:36:03 +0200

    Initial Commit

---
+++ Squaz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Squaz
+//
+//  Created by macbook on 16/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Squaz/SquazApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquazApp.swift
+//  Squaz
+//
+//  Created by macbook on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SquazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
